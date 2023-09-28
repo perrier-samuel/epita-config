a4f36bf6327707363ad4198b2944a3f812f62981
Samuel PERRIER <samuel.perrier@epita.fr>
2023-09-28 13:10:47 +0200

    add startup.sh

---
+++ startup.sh
@@ -0,0 +1,5 @@
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+nix-channel --update
+export NIX_PATH="$HOME"/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+nix-shell '<home-manager>' -A install
+
